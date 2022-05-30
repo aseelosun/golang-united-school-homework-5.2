1f0c2334215cb8cc435cf98ebff759d7da9541f6
ATolegen <ATolegen@1cb.kz>
2022-05-30 12:03:40 +0600

    add if/else

--- cache.go
+++ cache.go
@@ -3,17 +3,24 @@ package cache
 import "time"
 
 type Cache struct {
+	cElement       string
+	cDeadlineExist bool
+	cDeadline      time.Time
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{
+		cElement:       make(map[string]string),
+		cDeadline:      make(map[string]time.Time),
+		cDeadlineExist: make(map[string]bool),
+	}
 }
 
 func (receiver) Get(key string) (string, bool) {
-
 }
 
 func (receiver) Put(key, value string) {
+
 }
 
 func (receiver) Keys() []string {
